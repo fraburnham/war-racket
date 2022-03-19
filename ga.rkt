eeb9e38cd9837eb8ed23fdd1fd51e4b59bf76a51
Frank Burnham <frank@n8git.dev>
2022-03-19 08:00:38 -0400

    GA stuff somewhat refined
    Some issues. Couldn't figure out how to get World to be refined. The
    type checker seemed to lose track of things that it once knew were
    valid Tribes...

--- ga.rkt
+++ ga.rkt
@@ -1,4 +1,4 @@
-#lang typed/racket
+#lang typed/racket #:with-refinements
 
 (require typed/racket/async-channel
          typed/racket/random
@@ -6,38 +6,116 @@
          (prefix-in red: "redcode.rkt")
          (prefix-in pmars: "pmars.rkt"))
 
-(define-type Tribe (Listof red:Warrior))
-(define-type World (Listof Tribe))
+(require/typed racket/vector
+  (vector-sort (All (a) (-> (Vectorof a)
+                            (-> a a Boolean)
+                            (Vectorof a)))))
 
 (struct citizen ((warrior : red:Warrior)
                  (name : String)
                  (filename : String)
-                 (points : pmars:Points)))
+                 (points : pmars:Points))
+  #:transparent)
 
-(: tribe-count Positive-Integer)
-(define tribe-count 10)
-
-(: tribe-winners Natural)
+(: tribe-winners (Refine (winners : Natural) (= winners 3)))
 (define tribe-winners 3)
 
-(: tribe-size Positive-Integer)
+(: tribe-count (Refine (count : Natural) (= count 10)))
+(define tribe-count 10)
+
+(: tribe-size (Refine (size : Natural) (= size 35)))
 (define tribe-size 35) ; TODO!: update the other stuff so the breeding program is still sensible
 
+(define-type Warriors (Refine (warriors : (Vectorof red:Warrior))
+                              (= 35 (vector-length warriors))))
+
+(define-type Tribe (Refine (tribe : (Vectorof citizen))
+                           (= 35 (vector-length tribe))))
+
+#;
+(define-type World (Refine (world : (Vectorof Tribe))
+                           (= 10 (vector-length world))))
+;; It seems like the refinement for Tribe is lost along the way
+;; not sure why yet
+(define-type World (Listof Tribe))
+
+(: list->warriors (-> (Listof red:Warrior) Warriors))
+(define (list->warriors w)
+  (let ((warriors (make-vector tribe-size (ann '() red:Warrior)))
+        (w (list->vector w)))
+    (vector-copy! warriors 0 w 0 tribe-size)
+    warriors))
+
+(: vector->warriors (-> (Vectorof red:Warrior) Warriors))
+(define (vector->warriors w)
+  (let ((warriors (make-vector tribe-size (ann '() red:Warrior))))
+    (vector-copy! warriors 0 w 0 tribe-size)
+    warriors))
+
+(: warriors->tribe (-> Warriors Tribe))
+(define (warriors->tribe warriors)
+  (let ((tribe (make-vector tribe-size (citizen '() "" "" 0))))
+    (: rec (-> Natural Void))
+    (define (rec i)
+      (when (< i tribe-size)
+        (let ((c : red:Warrior (vector-ref warriors i)))
+          (vector-set! tribe i (citizen c "" "" #f))
+          (rec (add1 i)))))
+    (rec 0)
+    tribe))
+
+(: vector->tribe (-> (Vectorof citizen) Tribe))
+(define (vector->tribe v)
+  (let ((tribe (make-vector tribe-size (citizen '() "" "" 0))))
+    (vector-copy! tribe 0 v 0 tribe-size)
+    tribe))
+
+;; this can become an update only action! It'll take a tribe in and update them
+(: save-tribe (-> Tribe Natural Tribe))
+(define (save-tribe tribe offset)
+  (: rec (-> Natural Void))
+  (define (rec i)
+    (when (< i tribe-size)
+      (let ((c : citizen (vector-ref tribe i))
+            (name : String (number->string (+ i (* 1000 offset)))))
+        (vector-set! tribe
+                     i
+                     (struct-copy citizen
+                                  c
+                                  (name name)
+                                  (filename (gen:save-warrior (citizen-warrior c) name)))))
+      (rec (add1 i))))
+  (rec 0)
+  tribe)
+
+(: tribe-sort (-> Tribe (-> citizen citizen Boolean) Tribe))
+(define (tribe-sort tribe proc)
+  (let ((sorted (make-vector tribe-size (citizen '() "" "" 0))))
+    (vector-copy! sorted 0 (vector-sort tribe proc) 0 tribe-size)
+    sorted))
+
 ;; I think there is a logic failure in how fitness is being calculated...
-(: fitness (-> (Listof red:Warrior) (#:tribe-id-offset Integer) (Listof citizen)))
-(define (fitness population #:tribe-id-offset (offset 1))
-  (let* ((population : (Listof citizen) (map (lambda ((c : red:Warrior) (i : Integer)) : citizen
-                                               ;; this could be much better. I could be using new directories I think...
-                                               (let ((i : String (number->string (+ i (* 1000 offset)))))
-                                                 (citizen c i (gen:save-warrior c i) #f)))
-                                             population
-                                             (range (length population)))))
-    (sort
-     (map (lambda ((c : citizen) (p : pmars:Points)) : citizen
-            (struct-copy citizen c (points p)))
-          population
-          (pmars:parse-result (pmars:run (map citizen-filename population))))
-     (lambda ((a : citizen) (b : citizen))
+;; maybe the issue is related to how scoring is only per tribe so a 4k score may only mean
+;; the rest of the tribe was SUPER weak. That makes me think I should have a round of fitness
+;; where the winners compete against each other so a champion can be found...
+;; also may not be worth writing out the intermediate winners instead of just the final ones...
+;; also should consider letting the champion(s) outbreed the rest of the population...
+(: fitness (-> Tribe (#:tribe-id-offset Natural) Tribe))
+(define (fitness tribe #:tribe-id-offset (offset 1))
+  (let* ((tribe : Tribe (save-tribe tribe offset)))
+    (vector-copy! ; pop the output back in tribe so the checker can tell it's still a tribe
+     tribe
+     0
+     ;; pull this fn out as attach-fitness or something
+     (vector-map (lambda ((c : citizen) (p : pmars:Points)) : citizen
+                   (struct-copy citizen c (points p)))
+                 tribe
+                 (list->vector (pmars:parse-result (pmars:run (vector->list (vector-map citizen-filename tribe))))))
+     0
+     tribe-size)
+    (tribe-sort
+     tribe
+     (lambda ((a : citizen) (b : citizen)) : Boolean
        (let* ((a-points : pmars:Points (citizen-points a))
               (a-points : Integer (if (integer? a-points) a-points 0))
               (b-points : pmars:Points (citizen-points b))
@@ -50,67 +128,118 @@
     (let ((rest : (-> red:Warrior red:Warrior)
                 (lambda (a)
                   (if (empty? a) '() (rest a)))))
-      (letrec ((rec : (-> red:Warrior red:Warrior red:Warrior)
-                    (lambda (a b)
-                      (if (and (empty? a) (empty? b))
-                          '()
-                          (if (> (random) 0.5)
-                              (if (empty? a)
-                                  (rec a (rest b))
-                                  (cons (first a) (rec (rest a) (rest b))))
-                              (if (empty? b)
-                                  (rec (rest a) b)
-                                  (cons (first b) (rec (rest a) (rest b)))))))))
-        (rec a b)))))
+      (: rec (-> red:Warrior red:Warrior red:Warrior))
+      (define (rec a b)
+        (if (and (empty? a) (empty? b))
+            '()
+            (if (> (random) 0.5)
+                (if (empty? a)
+                    (rec a (rest b))
+                    (cons (first a) (rec (rest a) (rest b))))
+                (if (empty? b)
+                    (rec (rest a) b)
+                    (cons (first b) (rec (rest a) (rest b)))))))
+      (rec a b))))
 
-(: breed (-> (Listof red:Warrior) (Listof red:Warrior)))
+;; TODO: Refine me!
+(: breed (-> (Vectorof red:Warrior) (Vectorof red:Warrior)))
 (define (breed warriors)
-  (letrec ((rec : (-> (Listof red:Warrior) (Listof red:Warrior) (Listof red:Warrior))
+  ;; this could use list->warriors and an intermediate list I think
+  (letrec ((rec : (-> (Vectorof red:Warrior) (Vectorof red:Warrior) (Vectorof red:Warrior))
                 (lambda (warriors children)
-                  (if (= 1 (length warriors))
+                  (if (= 1 (vector-length warriors))
                       children
-                      (let ((rest (rest warriors))
-                            (parent (first warriors)))
-                        (rec rest (append (map (mate parent) rest) children)))))))
-    (rec warriors '())))
+                      (let ((rest (vector-drop warriors 1))
+                            (parent : red:Warrior (vector-ref warriors 0)))
+                        (rec rest (vector-append
+                                   (vector-map (mate parent) rest)
+                                   children)))))))
+    (rec warriors '#())))
 
 (: new-world (-> World))
 (define (new-world)
-  (map (lambda (_) (gen:warriors tribe-size 100)) (range tribe-count)))
+  (map (lambda (_) : Tribe
+         (warriors->tribe (list->warriors (gen:warriors tribe-size 100))))
+       (range tribe-count)))
 
-;; TODO: should also take the very worst performer for genetic diversity
+;; this name is terrible
+(: tribe-thread (-> (Async-Channelof Tribe) Tribe Natural Thread))
+(define (tribe-thread result-chan tribe offset)
+  (thread
+   (thunk
+    (async-channel-put result-chan (fitness tribe #:tribe-id-offset offset)))))
+
+;; still a terrible name...
+(: world-threads (-> (Async-Channelof Tribe) World (Listof Thread)))
+(define (world-threads result-chan world)
+  (if (empty? world)
+      '()
+      (cons
+       (tribe-thread result-chan (first world) (length world))
+       (world-threads result-chan (rest world)))))
+
+;; a bad name, too. maybe `select-breeders`?
+(: world-best (-> World (Vectorof citizen)))
+(define (world-best world)
+  ;; TODO: should also take the very worst performer for genetic diversity
+  (apply vector-append
+         (map (lambda ((tribe : Tribe)) : (Vectorof citizen)
+                (vector-take tribe tribe-winners))
+              world)))
+
+(: save-citizens (-> (Vectorof citizen) Void))
+(define (save-citizens citizens)
+  (for ((c citizens))
+    (gen:save-warrior (citizen-warrior c)
+                      (string-append "warriors/" ; format string would probably be more clear...
+                                     (number->string (or (citizen-points c) 0))
+                                     "-"
+                                     (citizen-name c)
+                                     "-"
+                                     (number->string (random 1 100))))))
+
+;; there should be a way to convince the checker that the offspring vector
+;; is larger than the chunk size...
+#;
+(: vector-chunk (All (a) (-> ((v : (Vectorof a))
+                              (n : (v) (Refine (chunk-size : Natural)
+                                               (< chunk-size (vector-length v)))))
+                             (Vectorof (Vectorof a)))))
+(: vector-chunk (All (a) (-> (Vectorof a) Natural (Vectorof (Vectorof a)))))
+;; NB: Chunks < chunk-size are dropped
+(define (vector-chunk v chunk-size)
+  (if (> chunk-size (vector-length v))
+      '#()
+      (vector-append
+       (vector (vector-take v chunk-size))
+       (vector-chunk (vector-drop v chunk-size) chunk-size))))
+
+(: offspring->world (-> (Vectorof red:Warrior) World))
+(define (offspring->world offspring)
+  (map
+   (lambda ((pseudo-warriors : (Vectorof red:Warrior))) : Tribe
+     (warriors->tribe (vector->warriors pseudo-warriors)))
+   (vector->list (vector-chunk offspring tribe-size))))
+
 (: world-step (-> World World))
-(define (world-step tribes)
-  (let* ((chan : (Async-Channelof (Listof citizen)) (make-async-channel tribe-count))
-         (threads (map (lambda ((tribe : (Listof red:Warrior)) (offset : Integer))
-                         (thread (lambda ()
-                                   (async-channel-put chan (fitness tribe #:tribe-id-offset offset)))))
-                       tribes
-                       (range tribe-count))))
+(define (world-step world)
+  ;; TODO: champion phase where the bests compete so that champions can breed more aggressively?
+  ;; TODO: I want to be able to tell which generation the saved warriors are from
+  (let* ((chan : (Async-Channelof Tribe) (make-async-channel (ann tribe-count Exact-Positive-Integer)))
+         (threads (world-threads chan world)))
     (for ((t threads))
       (thread-wait t))
-    (let* ((tribes : (Listof (Listof citizen)) (map (lambda (x) (async-channel-get chan)) (range tribe-count)))
-           (best : (Listof citizen) (apply append (map (lambda ((citizens : (Listof citizen))) : (Listof citizen)
-                                                         (take citizens tribe-winners))
-                                                       tribes)))
-           (offspring : (Listof red:Warrior) (breed (map citizen-warrior best))))
-      (for ((c best))
-        (gen:save-warrior (citizen-warrior c)
-                          (string-append "warriors/" ; format string would probably be more clear...
-                                         (number->string (or (citizen-points c) 0))
-                                         "-"
-                                         (citizen-name c)
-                                         "-"
-                                         (number->string (random 1 100)))))
-      (letrec ((partition : (-> (Listof red:Warrior) World)
-                          (lambda (warriors)
-                            (if (empty? warriors)
-                                '()
-                                (cons (take warriors tribe-size) (partition (drop warriors tribe-size)))))))
-        (partition (random-sample offspring (* tribe-size tribe-count)))))))
+    (let* ((tribes : World (map (lambda (x) (async-channel-get chan)) (range tribe-count))) ; make this `world` if it doesn't shadow anything important
+           (best : (Vectorof citizen) (world-best tribes))
+           (offspring : (Vectorof red:Warrior) (breed (vector-map citizen-warrior best))))
+      (save-citizens best)
+      (take (offspring->world offspring) tribe-count))))
 
 (: evolution (-> Natural World))
 (define (evolution number-of-generations)
   (if (zero? number-of-generations)
       (new-world)
       (world-step (evolution (sub1 number-of-generations)))))
+
+;; so now I think the final step may need to involve competing against intentionally built warriors
+;; to weed out high scores caused by ties (maybe higher round counts are the answer?)
